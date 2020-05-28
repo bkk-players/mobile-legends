79da6d529e34b07467c73a2eb031fdccfb6a30b2
mario-viegash <mario.viegash@traveloka.com>
2020-05-28 21:27:28 +0700

    Updates

---
+++ precache-manifest.e61aaa1d59b9971e65a4e5fbb7c7201e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c39b0bc543a63470abe748ef015e4c",
+    "url": "/mobile-legends/index.html"
+  },
+  {
+    "revision": "97edf566aef81a348957",
+    "url": "/mobile-legends/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9d261762f6b6a01bba1",
+    "url": "/mobile-legends/static/js/2.03ca2442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-legends/static/js/2.03ca2442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97edf566aef81a348957",
+    "url": "/mobile-legends/static/js/main.2a272924.chunk.js"
+  },
+  {
+    "revision": "53fe2f0b9220ee429cbd",
+    "url": "/mobile-legends/static/js/runtime-main.46bcc3bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobile-legends/static/media/logo.5d5d9eef.svg"
+  }
+]);
